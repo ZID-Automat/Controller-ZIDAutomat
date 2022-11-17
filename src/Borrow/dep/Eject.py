0789926292af5251377d878b706dfc5c4534af88
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2022-11-17 22:31:47 +0100

    Projekt Struktur

---
+++ src/Borrow/dep/Eject.py
@@ -0,0 +1,11 @@
+from ast import Num
+from xmlrpc.client import boolean
+
+# class reponsible for ejecting an Item
+class Eject:
+    def __init__(self) -> None:
+        pass
+
+#hier sollte wahrscheinlich statt der ItemId, eine Position oder so sein.
+    def eject(self,ItemId:int)-> boolean:
+        pass
