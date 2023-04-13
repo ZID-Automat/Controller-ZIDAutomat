e19d0acf060b8ded975d4fb2551771f3dbcbe1c3
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2023-04-13 22:16:24 +0200

    working Eject

--- src/Borrow/dep/Eject.py
+++ src/Borrow/dep/Eject.py
@@ -6,49 +6,40 @@ from time import sleep
 
 from machine import Pin, PWM
 
-ClockPin = 34
-Pin1 = 13
-Pin2 = 12
-Pin3 = 14
-Pin4 = 27
-Pin5 = 26
-class Eject:
-    def __init__(self, low, high, act) -> None:
-        self.clock = ADC(Pin(ClockPin))
-        self.clock.atten(ADC.ATTN_11DB)
-        self.Pins = [
-            Pin(Pin1,Pin.OUT),
-            Pin(Pin2,Pin.OUT),
-            Pin(Pin3,Pin.OUT),
-            Pin(Pin4,Pin.OUT),
-            Pin(Pin5,Pin.OUT) 
-        ]
-        self.low = low
-        self.high = high
-        self.act = act
+ClockPin1 = 32
+ClockPin2 = 33
 
-        for p in self.Pins:
-            p.on()
-     
+Pin1 = 13#F
+Pin2 = 12#A und G
+Pin3 = 14#B und H
+Pin4 = 27#C 
+Pin5 = 26#D
+Pin6 = 25#E
+
+
+
+class Eject:
+    def __init__(self) -> None:
+        pass
 
     def eject(self,Location:str)-> int:
         self.PressCode(Location)
         return True
 
     def PressCode(self,str:str):
+
         Button = {
-            "A":(0,20),
-            "B":(1,20),
-            "D":(2,20),
-            "E":(3,20),
-            "F":(4,20),
-            "G":(0,30),
-            "H":(1,30),
-            "I":(2,30),
-            "J":(3,30)
+            "A":(Pin2,ClockPin1),
+            "B":(Pin3,ClockPin1),
+            "C":(Pin4,ClockPin1),
+            "D":(Pin5,ClockPin1),
+            "E":(Pin6,ClockPin1),
+            "F":(Pin1,ClockPin2),
+            "G":(Pin2,ClockPin2),
+            "H":(Pin3,ClockPin2),
         }
         for i in str:
-            self.AusgabePin(self.Pins[0],self.clock,Button[i][1])
+            self.HGFDings(Button[i][0],Button[i][1])
 
     def AusgabePin(self,pin, clock, Laenge:int): 
         print("Waiting for Clock to not be zero")
@@ -81,15 +72,10 @@ class Eject:
 
         print("PinSignal was send")
 
-    def test(self, sl, edge, length):
-        out = Pin(5, Pin.OUT)
-        clockRead = ADC(Pin(32))
-
-        ClockOut = PWM(Pin(4), 5000)
-
-
+    def HGFDings(self, pin,clock, sl = 0, edge = 0.1, length = 0.1):
+        out = Pin(pin, Pin.OUT)
+        clockRead = ADC(Pin(clock))
         while True:
-            val = (3.3/4095)* self.clock.read()
             ClockVal = (3.3/4095)* clockRead.read()
 
             if ClockVal > edge:
@@ -97,10 +83,8 @@ class Eject:
             else:
                 out.off()
                 sleep(length)
-                Pin(5, Pin.IN)
+                Pin(pin, Pin.IN)
                 return
-
-            ClockOut.duty(int((1023/3.3)*ClockVal))
 
             sleep_ms(sl)
 
