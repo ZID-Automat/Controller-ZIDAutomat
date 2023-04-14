27e64c0ecb9e7971a49801ba79aee33c4f4bccf8
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2023-04-14 20:23:05 +0200

    finally completly working

--- src/Borrow/dep/Eject.py
+++ src/Borrow/dep/Eject.py
@@ -19,15 +19,16 @@ from machine import Pin, PWM
 
 
 class Eject:
-    def __init__(self, Pins, ClockPins) -> None:
+    def __init__(self, Pins = [13,12,14,27,26,25], ClockPins = 34, RelePin = 15,InvertRele = 1) -> None:
         self.Pin1 = Pins[0]
         self.Pin2 = Pins[1]
         self.Pin3 = Pins[2]
         self.Pin4 = Pins[3]
         self.Pin5 = Pins[4]
         self.Pin6 = Pins[5]
-        self.ClockPin1 = ClockPins[0]
-        self.ClockPin2 = ClockPins[1]
+        self.ClockPin1 = ClockPins
+        self.RelePin = RelePin
+        self.InvertRele = InvertRele
         pass
 
     def eject(self,Location:str)-> int:
@@ -37,26 +38,31 @@ class Eject:
 
     def PressCode(self,stri:str):
         Button = {
-            "A":(self.Pin2,self.ClockPin1),
-            "B":(self.Pin3,self.ClockPin1),
-            "C":(self.Pin4,self.ClockPin1),
-            "D":(self.Pin5,self.ClockPin1),
-            "E":(self.Pin6,self.ClockPin1),
-            "F":(self.Pin1,self.ClockPin2),
-            "G":(self.Pin2,self.ClockPin2),
-            "H":(self.Pin3,self.ClockPin2),
+            "A":(self.Pin2,1),
+            "B":(self.Pin3,1),
+            "C":(self.Pin4,1),
+            "D":(self.Pin5,1),
+            "E":(self.Pin6,1),
+            "F":(self.Pin1,0),
+            "G":(self.Pin2,0),
+            "H":(self.Pin3,0),
         }
-        self.HGFDings(Button[stri][0],Button[stri][1])
+        self.HGFDings(Button[stri][0],self.ClockPin1, self.RelePin,Button[stri][1])
 
-    def HGFDings(self, pin,clock, sl = 0, edge = 0.1, length = 0.1):
+    def HGFDings(self, pin,clock,relePin,ReleVal,offi = 0, sl = 0, edge = 0.1, length = 0.1):
         out = Pin(pin, Pin.OUT)
+        
+        RelePin = Pin(relePin, Pin.OUT)
+        RelePin.value(ReleVal* self.InvertRele)
+
         clockRead = ADC(Pin(clock))
+
         while True:
             ClockVal = (3.3/4095)*clockRead.read()
-
             if ClockVal > edge:
                 out.on()
             else:
+                sleep(offi)
                 out.off()
                 sleep(length)
                 Pin(pin, Pin.IN)
